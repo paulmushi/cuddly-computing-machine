29be07a8ce13ac43964b03db0a6b48714dea5404
Paul Mushi <90787935+paulmushi@users.noreply.github.com>
2023-04-27 18:36:22 +0300

    Update api.php
    Removing my API Keys

--- php/api.php
+++ php/api.php
@@ -2,7 +2,7 @@
 ini_set("display_errors", 0);
 
 // Set up API key, URL, and model
-$API_KEY = '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';
+$API_KEY = '';
 $url = 'https://api.openai.com/v1/completions';
 $model = "text-davinci-003";
 $chat = "";
